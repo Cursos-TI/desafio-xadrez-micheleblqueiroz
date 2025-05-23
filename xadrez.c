7a7797813f01d81dfa6f3dbbd0405cacca096954
micheleblqueiroz <micheleblqueiroz86@gmail.com>
2025-05-23 17:56:59 +0000

    xadrez final

--- xadrez.c
+++ xadrez.c
@@ -6,50 +6,92 @@
 #define DIREITA "Direita\n"
 #define DIAGONAL_SUP_DIR "Cima e Direita\n"
 
-#define BISPO_MOV 5    
+#define BISPO_MOV 5   
 #define TORRE_MOV 5    
-#define RAINHA_MOV 8  
-#define CAVALO_MOV 3  // Total moves for Knight: 2 down, 1 left
+#define RAINHA_MOV 8   
+#define CAVALO_MOV 3   
+
+
+void moveBispo(int move, int max_moves) {
+    if (move > max_moves) return; 
+    
+
+    int vertical;
+    for (vertical = 0; vertical < 1; vertical++) { 
+        int horizontal = 0;
+        while (horizontal < 1) { 
+            printf("%d: %s", move, DIAGONAL_SUP_DIR);
+            horizontal++;
+        }
+    }
+    
+    
+    moveBispo(move + 1, max_moves);
+}
+
+
+void moveTorre(int move, int max_moves) {
+    if (move > max_moves) return; 
+    
+    printf("%d: %s", move, DIREITA);
+    moveTorre(move + 1, max_moves); 
+}
+
+
+void moveRainha(int move, int max_moves) {
+    if (move > max_moves) return;
+    
+    printf("%d: %s", move, ESQUERDA);
+    moveRainha(move + 1, max_moves); 
+}
 
 int main() {
-    int i;
-
-    // Bishop movement
+   
     printf("Movimentacao do Bispo:\n");
-    for (i = 0; i < BISPO_MOV; i++) {
-        printf("%d: %s", i + 1, DIAGONAL_SUP_DIR);
-    }
+    moveBispo(1, BISPO_MOV);
     printf("\n");
 
-    // Rook movement
+   
     printf("Movimentacao da Torre:\n");
-    i = 0;
-    while (i < TORRE_MOV) {
-        printf("%d: %s", i + 1, DIREITA);
-        i++;
-    }
+    moveTorre(1, TORRE_MOV);
     printf("\n");
 
-    // Queen movement
+   
     printf("Movimentacao da Rainha:\n");
-    i = 0;
-    do {
-        printf("%d: %s", i + 1, ESQUERDA);
-        i++;
-    } while (i < RAINHA_MOV);
+    moveRainha(1, RAINHA_MOV);
     printf("\n");
 
-    // Knight movement
+   
     printf("Movimentacao do Cavalo:\n");
-    // Using nested loops to simulate the "L" shape movement
     int move_count = 0;
-    for (int down = 0; down < 2; down++) {  // Loop for 2 downward moves
-        printf("%d: %s", ++move_count, BAIXO);
-        // Nested while loop for clarity, though not strictly necessary here
-        int left = 0;
-        while (left < 1 && down == 1) {  // Only move left after 2nd down move
-            printf("%d: %s", ++move_count, ESQUERDA);
-            left++;
+    int max_up = 2; 
+    int max_right = 1; 
+    int up_count = 0;
+    
+    
+    for (int i = 0; i < CAVALO_MOV; i++) {
+        
+        if (up_count >= max_up) {
+            continue; 
+        }
+        
+        
+        int j = 0;
+        while (j < 1) {
+            if (up_count < max_up) {
+                printf("%d: %s", ++move_count, CIMA);
+                up_count++;
+            } else {
+                
+                printf("%d: %s", ++move_count, DIREITA);
+                break; 
+            }
+            j++;
+        }
+        
+        
+        if (move_count >= CAVALO_MOV) {
+            break;
         }
     }
     printf("\n");
